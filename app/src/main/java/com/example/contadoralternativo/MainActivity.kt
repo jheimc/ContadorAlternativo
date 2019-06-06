2fcd725b722143c721801e6a96a1d560fea5ef17
jheimc <jheimymiranda@gmail.com>
2019-06-05 20:32:54 -0400

    tarea terminada esta vez si xD

--- app/src/main/java/com/example/contadoralternativo/MainActivity.kt
+++ app/src/main/java/com/example/contadoralternativo/MainActivity.kt
@@ -34,7 +34,13 @@ class MainActivity : AppCompatActivity() {
 
 
                 contadorNuevo +=1
-                texto?.append("usuario: ${entradaUsuario.text.toString()}!!  boton se ha clickeado $contadorNuevo vez\n")
+                if(contadorNuevo== 1){
+                    texto?.append("usuario: ${entradaUsuario.text.toString()}!!  boton se ha clickeado $contadorNuevo vez\n")
+                }else{
+                    texto?.append("usuario: ${entradaUsuario.text.toString()}!!  boton se ha clickeado $contadorNuevo veces\n")
+
+                }
+
 
                 if(contadorNuevo== 10){
                     Toast.makeText(this@MainActivity,"se clickeo el boton $contadorNuevo veces",Toast.LENGTH_SHORT).show()
